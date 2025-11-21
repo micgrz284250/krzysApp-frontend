cea8dea66a197fce9d2392955260795851fef113
Michał Grzebielec <284250@student.pwr.edu.pl>
2025-11-21 17:22:05 +0100

    implemented reactive chart of Krzys vitals

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,40 +1,100 @@
 <template>
   <Line
-      id="vitals-chart"
-      :options="chartOptions"
+      ref="vitalsChartRef"
       :data="chartData"
+      :options="chartOptions"
   />
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+import {Line} from 'vue-chartjs'
+import {Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip, CategoryScale} from 'chart.js'
+import {ref} from "vue";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
+defineExpose({ updateChart })
+
+const vitalsChartRef = ref(null)
+
 const chartData = ref({
-  labels: [],
+  labels: [0, 0, 0, 0, 0],
   datasets: [
     {
       label: 'Energia',
       backgroundColor: '#ff0000',
-      data: []
+      borderColor: '#ff0000',
+      data: [0, 0, 0, 0, 0]
     },
     {
       label: 'Tętno',
       backgroundColor: '#0000ff',
-      data: []
+      borderColor: '#0000ff',
+      data: [0, 0, 0, 0, 0]
     },
     {
       label: 'Temperatura',
       backgroundColor: '#00ff00',
-      data: []
+      borderColor: '#00ff00',
+      data: [0, 0, 0, 0, 0]
     }
   ]
-})
+});
 
 const chartOptions = {
   responsive: true
 }
+
+function updateChart(krzys) {
+
+  const newLabels = [... chartData.value.labels];
+  const newDataEnergia = [... chartData.value.datasets[0].data];
+  const newDataTetno = [... chartData.value.datasets[1].data];
+  const newDataTemperatura = [... chartData.value.datasets[2].data];
+
+  if (chartData.value.labels.length >= 5){
+    newLabels.shift()
+    newDataEnergia.shift()
+    newDataTetno.shift()
+    newDataTemperatura.shift()
+  }
+  newLabels.push(krzys.value.id);
+  chartData.value.labels = newLabels;
+  // Energia
+  newDataEnergia.push(krzys.value.energia);
+  chartData.value.datasets[0].data = newDataEnergia;
+  // Tętno
+  newDataTetno.push(krzys.value.tetno);
+  chartData.value.datasets[1].data = newDataTetno;
+  // Temperatura
+  newDataTemperatura.push(krzys.value.temperatura);
+  chartData.value.datasets[2].data = newDataTemperatura;
+
+  console.log(chartData.value);
+
+  chartData.value = {
+    labels: newLabels,
+    datasets: [
+      {
+        label: 'Energia',
+        backgroundColor: '#ff0000',
+        borderColor: '#ff0000',
+        data: newDataEnergia
+      },
+      {
+        label: 'Tętno',
+        backgroundColor: '#0000ff',
+        borderColor: '#0000ff',
+        data: newDataTetno
+      },
+      {
+        label: 'Temperatura',
+        backgroundColor: '#00ff00',
+        borderColor: '#00ff00',
+        data: newDataTemperatura
+      }
+    ]
+  }
+}
+
 </script>
--- src/components/Krzys.vue
+++ src/components/Krzys.vue
@@ -8,7 +8,7 @@
           <input class="message" v-model="message" type="text" @keyup.enter="postMessage($event.target.value)">
       </div>
       <div class="box">
-        <BarChart/>
+        <BarChart ref="BarChartRef" />
         <p>Parametry Krzysia</p>
         <section class="parameter">
           <p>Energia: {{Krzys.energia}}</p>
@@ -22,12 +22,13 @@
 </template>
 
 <script setup>
-import {onMounted, ref, useTemplateRef} from "vue";
+import {onMounted, ref} from "vue";
 import BarChart from "./Chart.vue";
 
 const API_URL = 'http://localhost:8080';
 
 const Krzys = ref({
+  id: 0,
   energia: 0,
   tetno: 0,
   temperatura: 0.0,
@@ -36,11 +37,15 @@ const Krzys = ref({
 
 const message = ref()
 
+const BarChartRef = ref(null);
+
 async function fetchVitals() {
   try {
     const response = await fetch(`${API_URL}/vitals`);
     Krzys.value = await response.json();
-    console.log(Krzys);
+    console.log(Krzys.value);
+    // przekażemy aktualne dane Krzysia do wykresu
+    BarChartRef.value.updateChart(Krzys);
   } catch (error) {
     console.log(error);
   }
